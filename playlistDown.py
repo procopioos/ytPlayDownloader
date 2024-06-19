387bdf0ee627b4e34cc72e0ecc4a2a256d72a094
Simone Procopio <procopiosimone9@gmail.com>
2024-06-19 10:56:41 +0200

    Added a GUI branch; First commit on the branch

--- playlistDown.py
+++ playlistDown.py
@@ -3,6 +3,9 @@ import requests
 import os
 from pytube import YouTube
 from pytube import Playlist as PyTubePlaylist
+import tkinter as tk
+from tkinter import ttk
+from tkinter.filedialog import askdirectory
 
 class Playlist:
     def __init__(self, playListUrl):
@@ -11,14 +14,14 @@ class Playlist:
         self.download_videos()
 
     def get_playlist_videos(self):
-        # Using PyTube's Playlist class to get video URLs from a playlist
         playlist = PyTubePlaylist(self.playListUrl)
         return playlist.video_urls
 
     def download_videos(self):
-        fwrite = open("playlist_detail.txt", "a") # Writing all songs from the Playlist on a text file
+        fwrite = open("playlist_detail.txt", "a")  # Writing all songs from the Playlist on a text file
         for i, url in enumerate(self.videos):
             try:
+                global yt
                 yt = YouTube(url)
                 video = yt.streams.get_highest_resolution()
                 print(f"Downloading: {yt.title} in {destination}")
@@ -28,6 +31,15 @@ class Playlist:
                 print(f"Error downloading video {i+1}: {e}")
         fwrite.close()
 
+def chooseLoc():
+    global destination
+    destination = askdirectory()
+
+def dwnload():
+    url = urlTxt.get("1.0", tk.END).strip()
+    if url:
+        Playlist(url)
+
 if __name__ == "__main__":
     print("        _   _____  _             _____                      _                 _           ")
     print("       | | |  __ \| |           |  __ \                    | |               | |          ")
@@ -36,7 +48,20 @@ if __name__ == "__main__":
     print(" | |_| | |_| |    | | (_| | |_| | |__| | (_) \ V  V /| | | | | (_) | (_| | (_| |  __/ |   ")
     print("  \__, |\__|_|    |_|\__,_|\__, |_____/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|\___|_|   ")
     print("   __/ |                    __/ |                                                         ")
-    print("  |___/                    |___/                                                      v1.0")
-    playlist_url = input('Playlist URL: ')
-    destination = input('Destination folder: ')
-    Playlist(playlist_url)
+    print("  |___/                    |___/                                                  v1.0-GUI")
+
+    root = tk.Tk()
+    root.geometry("380x170")
+    root.resizable(False, False)
+    root.title("ytPlayDownloader-GUI")
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="URL:").grid(column=0, row=0, sticky='W')
+    urlTxt = tk.Text(frm, height=1, width=30)
+    urlTxt.grid(column=0, row=1, sticky='W')
+    ttk.Label(frm, text="Destination folder:").grid(column=1, row=0, sticky='E', padx=10)
+    DestBTN = tk.Button(frm, text="Choose...", command=chooseLoc)
+    DestBTN.grid(column=1, row=1, sticky='E', padx=10)
+    DwnloadBTN = tk.Button(frm, text="Start download", command=dwnload)
+    DwnloadBTN.grid(column=0, row=2, sticky='W')
+    root.mainloop()
